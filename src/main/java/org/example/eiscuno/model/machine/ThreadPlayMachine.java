cf3764306b8feebc7324f1de1e326e5b6f90232e
Nathal25 <nathalia.ortiz.granada@correounivalle.edu.co>
2024-06-18 10:45:17 -0500

    Permite que el usuario coma cartas de la baraja

--- src/main/java/org/example/eiscuno/model/machine/ThreadPlayMachine.java
+++ src/main/java/org/example/eiscuno/model/machine/ThreadPlayMachine.java
@@ -37,11 +37,20 @@ public class ThreadPlayMachine extends Thread {
     private void putCardOnTheTable(){
         int index = (int) (Math.random() * machinePlayer.getCardsPlayer().size());
         Card card = machinePlayer.getCard(index);
+        Card carOnTable=table.getCurrentCardOnTheTable();
+        if(!carOnTable.getUrl().contains("cards-uno/skip")){
+            if(card.getValue().equals(carOnTable.getValue())||card.getColor().equals(carOnTable.getColor())){
+                System.out.println("Son iguales");
+            }
+        } else {
+            System.out.println("Es saltar turno");
+        }
         table.addCardOnTheTable(card);
         tableImageView.setImage(card.getImage());
         machinePlayer.getCardsPlayer().remove(index);
         System.out.println("Se añadió "+tableImageView.getImage());
     }
+
 
     public void setHasPlayerPlayed(boolean hasPlayerPlayed) {
         this.hasPlayerPlayed = hasPlayerPlayed;
